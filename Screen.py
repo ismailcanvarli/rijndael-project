a28af13e0edb2a22484a21b736105d6e180b6e3d
İsmail Can Varlı <icanvarli@outlook.com>
2024-04-26 02:09:06 +0300

    Update GUI in Screen.py

--- Screen.py
+++ Screen.py
@@ -12,21 +12,23 @@ def decrypt():
     result_text.insert(tk.END, "Çözülmüş Metin: " + plaintext + "\n")
 
 window = tk.Tk()
+window.title("Rijndael Şifreleme")
+window.geometry("700x700")
 
-key_label = tk.Label(window, text="Anahtar:")
+key_label = tk.Label(window, text="Anahtar: (Key)", font=("Times New Roman", 12))
 key_label.pack(pady=5)
-key_entry = tk.Entry(window, width=50)
+key_entry = tk.Entry(window, width=50, font=("Times New Roman", 12))
 key_entry.pack(pady=15)
 
-text_label = tk.Label(window, text="Metin:")
+text_label = tk.Label(window, text="Metin: (Plain or Chipper text)", font=("Times New Roman", 12))
 text_label.pack(pady=5)
-text_entry = tk.Entry(window, width=50)
+text_entry = tk.Entry(window, width=50, font=("Times New Roman", 12))
 text_entry.pack(pady=15)
 
-encrypt_button = tk.Button(window, text="Şifrele", command=encrypt, width=10)
+encrypt_button = tk.Button(window, text="Şifrele", command=encrypt, width=10, font=("Times New Roman", 12,"bold"))
 encrypt_button.pack(pady=5)
 
-decrypt_button = tk.Button(window, text="Şifre Çöz", command=decrypt, width=10)
+decrypt_button = tk.Button(window, text="Şifre Çöz", command=decrypt, width=10, font=("Times New Roman", 12, "bold"))
 decrypt_button.pack(pady=5)
 
 result_text = tk.Text(window)
